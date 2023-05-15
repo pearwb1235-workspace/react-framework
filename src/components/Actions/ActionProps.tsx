a19baba3aaaeb8a1ebb02abd10c8a79a842464f1
梨水竹 <46672356+pearwb1235@users.noreply.github.com>
2023-05-15 16:42:24 +0800

    chore: 補齊匯入

--- src/components/Actions/ActionProps.tsx
+++ src/components/Actions/ActionProps.tsx
@@ -1,5 +1,6 @@
 import { ButtonTypeMap } from "@mui/material";
 import { DefaultComponentProps } from "@mui/material/OverridableComponent";
+import React from "react";
 
 export type ActionProps = DefaultComponentProps<ButtonTypeMap> & {
   text?: string;
